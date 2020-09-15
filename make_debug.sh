e1bb7921f9bf038af3c19d05a34ebdeff7441b7a
adiyoss <yossiadidrum@gmail.com>
2020-09-15 05:58:40 -0700

    remove code from web branch

--- make_debug.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-path=egs/debug/tr
-if [[ ! -e $path ]]; then
-    mkdir -p $path
-fi
-python3 -m denoiser.audio dataset/debug/noisy > $path/noisy.json
-python3 -m denoiser.audio dataset/debug/clean > $path/clean.json
